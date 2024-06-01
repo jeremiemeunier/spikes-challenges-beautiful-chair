efabf44e6c9a86777d73274289b65ceb819f6ef7
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-01 13:52:50 +0200

    Create Error404.jsx

---
+++ www/src/pages/error/Error404.jsx
@@ -0,0 +1,10 @@
+const Error404 = () => {
+  return (
+    <div className="page">
+      <h1>Oups !</h1>
+      <p>Cette page n'est plus disponible.</p>
+    </div>
+  );
+};
+
+export default Error404;
