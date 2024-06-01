28a49cc956835270e0c5a3074378489db7882fc7
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-01 11:33:52 +0200

    Update App.jsx

--- www/src/App.jsx
+++ www/src/App.jsx
@@ -1,23 +1,32 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
+
+// context
 import ScrollProvider from "./context/ContextScroll";
 
-function App() {
-  const [count, setCount] = useState(0);
+// components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+// pages
+import Home from "./pages/Home";
+import Error404 from "./pages/error/Error404";
+import Product from "./pages/Product";
+
+const App = () => {
   return (
     <ScrollProvider>
       <Router>
         <Header />
-        <Toast />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<Product />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
         <Footer />
       </Router>
     </ScrollProvider>
   );
-}
+};
 
 export default App;
