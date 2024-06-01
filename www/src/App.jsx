8cc245219fe8e487d0fc4002016229f8bb27740c
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-01 11:52:57 +0200

    Update App.jsx

--- www/src/App.jsx
+++ www/src/App.jsx
@@ -9,7 +9,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 // pages
-import Home from "./pages/Home";
 import Error404 from "./pages/error/Error404";
 import Product from "./pages/Product";
 
@@ -19,8 +18,7 @@ const App = () => {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<Product />} />
+          <Route path="/" element={<Product />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
         <Footer />
